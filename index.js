40105d8abcf661270f1a39491060a8eaa20da643
Lee Thanh <minhthanh@onename.net>
2023-12-02 16:33:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const telegramToken = '6610438736:AAFNPlGGY_SfuINu3dXThuyMFSOkoy1BpAg';
-const cloudflareToken = 'NB-sxyNA3JSyhezy2fUnTnXc3Pf7i8X17BJfBwLx';
+const telegramToken = '{YOUR_TELEGRAM_TOKEN}';
+const cloudflareToken = '{YOUR_CLOUDFLARE_TOKEN}';
 
 const bot = new TelegramBot(telegramToken, {polling: true});
 
@@ -12,7 +12,7 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const userMessage = msg.text;
 
-  axios.post('https://api.cloudflare.com/client/v4/accounts/d888e02cee618e93b19cd9e7717da715/ai/run/@cf/meta/llama-2-7b-chat-int8', {
+  axios.post('https://api.cloudflare.com/client/v4/accounts/{YOUR_ACCOUNT_ID}/ai/run/@cf/meta/llama-2-7b-chat-int8', {
     prompt: userMessage
   }, {
     headers: {
